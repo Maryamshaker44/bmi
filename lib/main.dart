a6a3f801d6d9adc4a4e3e5c5af01385114ace92a
Maryamshaker44 <101062701+Maryamshaker44@users.noreply.github.com>
2025-01-06 09:25:51 +0200

    upload

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:bmi/myapp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
---
+++ lib/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:bmi/feature/home/view/screens/homescreen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
